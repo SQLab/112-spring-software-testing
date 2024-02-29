69aebc414316140b4b55870c93ce164c925aa57e
HanHongChen <127834396+HanHongChen@users.noreply.github.com>
2024-02-29 07:43:25 +0000

    tmp

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,11 +4,26 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    //check fail case
+    assert.strictEqual(myClass.addStudent({}), -1);
+    //check success case
+    assert.strictEqual(myClass.addStudent(new Student), 0);
+
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
+    const myClass = new MyClass();
+    const names = ['John', 'Jane', 'Doe', 'Smith'];
+    names.forEach(name => {
+        const student = new Student();
+        student.setName(name);
+        const newStudentId = myClass.addStudent(student);
+        const newStudentName = myClass.getStudentById(newStudentId).getName();
+        console.log('[+] Added student with id: %d, name: %s', newStudentId, newStudentName);
+    });
+    assert.strictEqual(myClass.getStudentById())
     throw new Error("Test not implemented");
 });
 
