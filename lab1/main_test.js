2f9f3c35b364741324028b739fbfdc4abf9556e5
emily <emily@emily-1.myguest.virtualbox.org>
2024-02-29 22:04:46 +0800

    submit lab1

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,21 +3,45 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const jason = new Student();
+    const mary = new Student();
+    assert.strictEqual(0, myClass.addStudent(jason));
+    assert.strictEqual(1, myClass.addStudent(mary));
+    assert.strictEqual(-1, myClass.addStudent(123));
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    const names = ['John', 'Jane', 'Doe', 'Smith'];
+    names.forEach(name => {
+        const student = new Student();
+        student.setName(name);
+        const newStudentId = myClass.addStudent(student);
+        const newStudentName = myClass.getStudentById(newStudentId).getName();
+    });
+    assert.strictEqual(myClass.getStudentById(-2), null);
+    assert.strictEqual(myClass.getStudentById(0).getName(), 'John');
+    assert.strictEqual(myClass.getStudentById(1).getName(), 'Jane');
+    assert.strictEqual(myClass.getStudentById(2).getName(), 'Doe');
+    assert.strictEqual(myClass.getStudentById(3).getName(), 'Smith');
+    assert.strictEqual(myClass.getStudentById(4), null);
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student1 = new Student();
+    const student2 = new Student();
+    student1.setName('Jason')
+    assert.strictEqual(student1.name, 'Jason');
+    student2.setName(234);
+    assert.strictEqual(student2.name, undefined);
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const student1 = new Student();
+    const student2 = new Student();
+    student1.setName('Mary');
+    assert.strictEqual(student1.getName(), 'Mary');
+    assert.strictEqual(student2.getName(), '');
 });
