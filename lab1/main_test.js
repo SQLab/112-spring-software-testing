92ad692b26bfa7de98deae9ecd52544001cc0242
108062204 <andydaniel146@gmail.com>
2024-02-29 19:12:23 +0800

    first try

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,20 +4,41 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
+    const myClass = new MyClass();
+    const student = new Student();
+    const name="Daniel";
+    student.setName(name);
+    assert.strictEqual(myClass.addStudent(student),1);
     throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
+    const myClass = new MyClass();
+    const student = new Student();
+    const name="Daniel";
+    student.setName(name);
+    const newStudentId=myClass.addStudent(student);
+    assert.strictEqual(myClass.getStudentById(newStudentId).getName(),"Daniel");
     throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
+    const myClass = new MyClass();
+    const student = new Student();
+    const name="Daniel";
+    student.setName(name);
+    assert.strictEqual(student.name,"Daniel");
     throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
+    const myClass = new MyClass();
+    const student = new Student();
+    const name="Daniel";
+    student.name=name;
+    assert.strictEqual(student.getName(),"Daniel")
     throw new Error("Test not implemented");
 });
