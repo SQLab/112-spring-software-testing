f25b3445483e7956c62ac3dea12db044502c7ce5
108062204 <andydaniel146@gmail.com>
2024-02-29 20:05:16 +0800

    almost finish

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -7,9 +7,10 @@ test("Test MyClass's addStudent", () => {
     const myClass = new MyClass();
     const student = new Student();
     const name="Daniel";
-    student.setName(name);
-    assert.strictEqual(myClass.addStudent(student),1);
-    throw new Error("Test not implemented");
+    student.name=name;
+    assert.strictEqual(myClass.addStudent(student),0);
+    assert.strictEqual(myClass.addStudent("Daniel"),-1);
+    //throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
@@ -17,28 +18,31 @@ test("Test MyClass's getStudentById", () => {
     const myClass = new MyClass();
     const student = new Student();
     const name="Daniel";
-    student.setName(name);
-    const newStudentId=myClass.addStudent(student);
-    assert.strictEqual(myClass.getStudentById(newStudentId).getName(),"Daniel");
-    throw new Error("Test not implemented");
+    student.name=name;
+    myClass.students.push(student);
+    assert.strictEqual(myClass.getStudentById(0).name,"Daniel");
+    assert.strictEqual(myClass.getStudentById(1),null);
+    //throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    const myClass = new MyClass();
     const student = new Student();
     const name="Daniel";
     student.setName(name);
     assert.strictEqual(student.name,"Daniel");
-    throw new Error("Test not implemented");
+    student.setName(123);
+    assert.strictEqual(student.name,"Daniel");
+    //throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    const myClass = new MyClass();
     const student = new Student();
     const name="Daniel";
     student.name=name;
-    assert.strictEqual(student.getName(),"Daniel")
-    throw new Error("Test not implemented");
+    assert.strictEqual(student.getName(),"Daniel");
+    student.name=undefined;
+    assert.strictEqual(student.getName(),"");
+    //throw new Error("Test not implemented");
 });
