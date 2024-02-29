6a2e4770ccbc9850dc89d2e07c9dbde670d52080
Eusden Lin <s960030@gmail.com>
2024-02-29 22:10:09 +0800

    coverage 100%

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,20 +4,33 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName('John');
+    myClass.addStudent(student);
+    assert.strictEqual(myClass.getStudentById(0), student);
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    const student = new Student();
+    student.setName('John');
+    myClass.addStudent(student);
+    assert.strictEqual(myClass.getStudentById(0), student);
+    assert.strictEqual(myClass.getStudentById(999), null);
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName('John');
+    assert.strictEqual(student.getName(), "John");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
-});
+    const student = new Student();
+    student.setName('John');
+    assert.strictEqual(student.getName(), "John");
+}); 
