cd9e1ab2860fe36ca37eb3549b4659371b912c2b
hsuanyu414 <93172849+hsuanyu414@users.noreply.github.com>
2024-02-29 15:30:46 +0800

    lab1 finish
    1. lab1 finish

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,20 +4,64 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    class_test = new MyClass();
+
+    // not Student instance as input
+    assert.strictEqual(class_test.addStudent(1), -1);
+    
+    // Student instance as input
+    student_test = new Student();
+    assert.strictEqual(class_test.addStudent(student_test), 0);
+    
+    // throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    class_test = new MyClass();
+    student_test = new Student();
+    class_test.addStudent(student_test);
+    
+    // <0 as input
+    assert.strictEqual(class_test.getStudentById(-1), null);
+    
+    // >= class.students.length as input
+    assert.strictEqual(class_test.getStudentById(class_test.students.length), null);
+    
+    // usual case
+    assert.strictEqual(class_test.getStudentById(0), student_test);
+    
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    student_test = new Student();
+    
+    // not string as input
+    student_test.setName(1);
+    assert.strictEqual(student_test.name, undefined);
+    
+    // usual case (string as input)
+    string = "John";
+    student_test.setName(string);
+    assert.strictEqual(student_test.name, string);
+    
+    // throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    student_test = new Student();
+    
+    // not string as input
+    student_test.setName(1);
+    assert.strictEqual(student_test.getName(), '');
+    
+    // usual case (string as input)
+    string = "John";
+    student_test.setName(string);
+    assert.strictEqual(student_test.getName(), string);
+    
+    // throw new Error("Test not implemented");
 });
