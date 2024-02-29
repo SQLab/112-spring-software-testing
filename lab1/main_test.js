f35c9d049ca2562c1e31707a8e13c890d49011f6
HanHongChen <127834396+HanHongChen@users.noreply.github.com>
2024-02-29 10:05:39 +0000

    finished lab1

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -15,24 +15,35 @@ test("Test MyClass's addStudent", () => {
 test("Test MyClass's getStudentById", () => {
     // TODO
     const myClass = new MyClass();
+    assert.strictEqual(myClass.getStudentById(-1), null);
+    assert.strictEqual(myClass.getStudentById(10), null);
     const names = ['John', 'Jane', 'Doe', 'Smith'];
     names.forEach(name => {
         const student = new Student();
         student.setName(name);
         const newStudentId = myClass.addStudent(student);
         const newStudentName = myClass.getStudentById(newStudentId).getName();
-        console.log('[+] Added student with id: %d, name: %s', newStudentId, newStudentName);
     });
-    assert.strictEqual(myClass.getStudentById())
-    throw new Error("Test not implemented");
+    assert.strictEqual(myClass.getStudentById(0).getName(), 'John');
+    assert.strictEqual(myClass.getStudentById(1).getName(), 'Jane');
+    assert.strictEqual(myClass.getStudentById(2).getName(), 'Doe');
+    assert.strictEqual(myClass.getStudentById(3).getName(), 'Smith');
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName(123);
+    assert.strictEqual(student.getName(), '');
+    student.setName('123');
+    assert.strictEqual(student.getName(), '123');
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const student = new Student();
+    student.setName(123);
+    assert.strictEqual(student.getName(), '');
+    student.setName('123');
+    assert.strictEqual(student.getName(), '123');
 });
