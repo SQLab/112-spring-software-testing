dde6f0e92cc43d15265ee981e0ed23096046c6c2
CTHua <illiew2470@gmail.com>
2024-02-29 15:16:23 +0800

    test: MyClass addStudent

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,8 +3,19 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    const myClass = new MyClass();
+    // if student is not an instance of Student, return -1
+    assert.strictEqual(myClass.addStudent({}), -1);
+
+    // normal case
+    const names = ['John', 'Jane', 'Doe', 'Smith'];
+    names.forEach(name => {
+        const student = new Student();
+        student.setName(name);
+        const newStudentId = myClass.addStudent(student);
+        const newStudentName = myClass.getStudentById(newStudentId).getName();
+        assert.strictEqual(newStudentName, name);
+    });
 });
 
 test("Test MyClass's getStudentById", () => {
