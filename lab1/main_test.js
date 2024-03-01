3eebb89ad37f591011a81db481d8dcee74e64c45
Kuan-Wei Chiu <visitorckw@gmail.com>
2024-03-01 22:31:50 +0800

    test: remove TODO comment and throwing unimplement error

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -3,21 +3,17 @@ const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
 });
 
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    
 });
