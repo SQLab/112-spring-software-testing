27f962199a56f0392b0a1bbb8d2bbe8a6a9b285d
tunajaw <t.a.w.forwork@gmail.com>
2024-03-04 21:15:40 +0800

    update files for 100% coverage

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,35 +4,47 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // Case 0
-    let testStudent = new Student();
-    let testClass = new MyClass();
-    testStudent.setName(userName='testStudent');
-    assert.strictEqual(testClass.addStudent(testStudent), 0);
-    // throw new Error("Test not implemented");
+    let testStudent0 = new Student();
+    let testClass0 = new MyClass();
+    testStudent0.setName(userName='testStudent');
+    assert.strictEqual(testClass0.addStudent(testStudent0), 0);
+
+    // Case 1
+    let testStudent1 = null;
+    let testClass1 = new MyClass();
+    assert.strictEqual(testClass1.addStudent(testStudent1), -1);
 });
 
 test("Test MyClass's getStudentById", () => {
-    let testStudent = new Student();
-    let testClass = new MyClass();
+    // Case 0
+    let testStudent0 = new Student();
+    let testClass0 = new MyClass();
 
-    testStudent.setName(userName='testStudent');
-    testClass.addStudent(testStudent);
+    testStudent0.setName(userName='testStudent');
+    testClass0.addStudent(testStudent0);
 
-    selectedStudent = testClass.getStudentById(0);
-    assert.strictEqual(selectedStudent.getName(), 'testStudent');
-    // throw new Error("Test not implemented");
+    selectedStudent0 = testClass0.getStudentById(0);
+    assert.strictEqual(selectedStudent0.getName(), 'testStudent');
+
+    // Case 1
+    selectedStudent1 = testClass0.getStudentById(1);
+    assert.strictEqual(selectedStudent1, null);
 });
 
 test("Test Student's setName", () => {
-    testStudent = new Student();
-    testStudent.setName(userName='testStudent');
-    assert.strictEqual(testStudent.getName(), 'testStudent');
-    //throw new Error("Test not implemented");
+    // Case 0
+    let testStudent0 = new Student();
+    testStudent0.setName(userName='testStudent');
+    assert.strictEqual(testStudent0.getName(), 'testStudent');
+
+    // Case 1
+    let testStudent1 = new Student();
+    testStudent1.setName(userName=123);
+    assert.strictEqual(testStudent1.getName(), '');
 });
 
 test("Test Student's getName", () => {
     testStudent = new Student();
     testStudent.setName(userName='testStudent');
     assert.strictEqual(testStudent.getName(), 'testStudent');
-    //throw new Error("Test not implemented");
 });
