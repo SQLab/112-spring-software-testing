ee20fbd9a327594ec44b187456ccd7cccec29145
楊咏倫 <yangyunglun@yangyonglundeMacBook-Air.local>
2024-03-05 11:13:48 +0800

    test push

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,20 +4,39 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const c = new MyClass();
+    const s1 = new Student();
+    const s2 = new Student();
+    assert.strictEqual(c.addStudent('1'), -1);
+    assert.strictEqual(c.addStudent(s1),0);
+    assert.strictEqual(c.addStudent(s2),1);
+
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const c = new MyClass();
+    const s1 = new Student();
+    assert.strictEqual(c.getStudentById(-1), null);
+
+    c.addStudent(s1);
+    assert.strictEqual(c.getStudentById(1),null);
+    assert.strictEqual(c.getStudentById(0),s1);
 });
 
 test("Test Student's setName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const s1 = new Student();
+    assert.strictEqual(s1.setName(1),undefined);
+    assert.strictEqual(s1.setName('a'),undefined);
+    
 });
 
 test("Test Student's getName", () => {
     // TODO
-    throw new Error("Test not implemented");
+    const s1 = new Student();
+    let n = 'Bob';
+    assert.strictEqual(s1.getName(n),'');
+    s1.setName('Bob');
+    assert.strictEqual(s1.getName(n),'Bob');
 });
