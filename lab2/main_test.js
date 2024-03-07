7fe1635152a0fbe48649df1124e865efceb16068
Bo Han Chen <b083040012@g-mail.nsysu.edu.tw>
2024-03-08 03:03:55 +0800

    feat: add 2 unit test for MailSystem

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -4,3 +4,25 @@ const { Application, MailSystem } = require('./main');
 
 // TODO: write your tests here
 // Remember to use Stub, Mock, and Spy when necessary
+
+test("Test MailSystem's write", () => {
+    const mailSystem = new MailSystem();
+    const result = mailSystem.write('James');
+    assert.strictEqual(result, 'Congrats, James!');
+});
+
+test("Test MailSystem's send", () => {
+
+    const mailSystem = new MailSystem();
+
+    // stub Math.random to return value <= 0.5
+    Math.random = () => 0.4;
+    const result1 = mailSystem.send('James', 'Congrats, James!');
+    assert.strictEqual(result1, false);
+    
+    // stub Math.random to return value > 0.5
+    Math.random = () => 0.6;
+    const result2 = mailSystem.send('James', 'Congrats, James!');
+    assert.strictEqual(result2, true);
+
+});
