08bb77e0d2d4114e5d9119a5614fe057b2cd3dbc
nyeee0622 <nyeee.cs12@nycu.edu.tw>
2024-03-07 19:02:45 +0800

    lab2 main update

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,6 +1,33 @@
 const test = require('node:test');
 const assert = require('assert');
 const { Application, MailSystem } = require('./main');
+const app = new Application();
 
-// TODO: write your tests here
-// Remember to use Stub, Mock, and Spy when necessary
+test('selected should be empty', () => {
+    app.getNames();
+    assert(app.selected == [], "Test not implemented");
+});
+
+test('should return a random person from the list', () => {
+    const randomPerson = app.getRandomPerson();
+
+    assert.include(app.people, randomPerson);
+});
+
+test('should select the next person and add to the selected list', () => {
+    const app = new Application();
+    const selectedPerson = app.selectNextPerson();
+
+    assert.include(app.selected, selectedPerson);
+});
+
+test('should return null when all people are selected', () => {
+    const app = new Application();
+
+    // Select all people
+    app.people.forEach(() => app.selectNextPerson());
+
+    const selectedPerson = app.selectNextPerson();
+
+    assert.isNull(selectedPerson);
+});
