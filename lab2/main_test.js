a46a43655121219405f7a86af4218ef4528f2d7e
Carlos <veture7275@gmail.com>
2024-03-13 21:43:05 +0800

    Update main_test.js

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -2,7 +2,7 @@ const test = require('node:test');
 const assert = require('assert');
 const fs = require('fs');
 const { Application, MailSystem } = require('./main');  
-
+ 
 // TODO: write your tests here 
 // Remember to use Stub, Mock, and Spy when necessary 
 // test("Test MailSystem's write", () => {
