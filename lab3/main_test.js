3e46de030f9a79b27ced33283724680f4d82e4f9
CTHua <illiew2470@gmail.com>
2024-03-14 15:14:21 +0800

    test: log test case

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,4 +1,4 @@
-const {describe, it} = require('node:test');
+const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
@@ -24,6 +24,35 @@ describe('Calculator.exp', () => {
         ];
         errorcases.forEach(errorcase => {
             assert.throws(() => calculator.exp(errorcase.param), {
+                name: errorcase.type,
+                message: errorcase.message,
+            });
+        });
+    })
+});
+
+describe('Calculator.log', () => {
+    it('return value', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: 48763, expected: 10.794727107543425 },
+            { param: 1, expected: 0 },
+        ];
+        testcases.forEach(testcase => {
+            assert.strictEqual(calculator.log(testcase.param), testcase.expected);
+        });
+    });
+
+    it('error', () => {
+        const calculator = new Calculator();
+        const errorcases = [
+            { param: NaN, type: 'Error', message: 'unsupported operand type' },
+            { param: Infinity, type: 'Error', message: 'unsupported operand type' },
+            { param: 1e-400, type: 'Error', message: 'math domain error (1)' },
+            { param: -1, type: 'Error', message: 'math domain error (2)' },
+        ];
+        errorcases.forEach(errorcase => {
+            assert.throws(() => calculator.log(errorcase.param), {
                 name: errorcase.type,
                 message: errorcase.message,
             });
