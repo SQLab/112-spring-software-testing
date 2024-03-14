168fd6d06043019ed2c81d6a51b5e83baefda73d
CTHua <illiew2470@gmail.com>
2024-03-14 12:34:48 +0800

    test: MailSystem testing

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,6 +1,26 @@
-const test = require('node:test');
+const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Application, MailSystem } = require('./main');
 
-// TODO: write your tests here
-// Remember to use Stub, Mock, and Spy when necessary
+describe('MailSystem', () => {
+    it('write', (t) => {
+        const mailSystem = new MailSystem();
+        const context = mailSystem.write('John');
+        assert.strictEqual(context, 'Congrats, John!');
+    });
+
+    it('send', (t) => {
+        const mailSystem = new MailSystem();
+
+        // Stub Math.random to always return 0.6
+        t.mock.method(Math, 'random', () => 0.6);
+        const success = mailSystem.send('John', 'Congrats, John!');
+        assert.strictEqual(success, true);
+
+        // Stub Math.random to always return 0.4
+        t.mock.method(Math, 'random', () => 0.4);
+        const failure = mailSystem.send('John', 'Congrats, John!');
+        assert.strictEqual(failure, false);
+    });
+});
+
