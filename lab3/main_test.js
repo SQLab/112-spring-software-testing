8b72f8a79d71ecb5f999cc3f84a8a0b929007426
poro777 <qaqaqqa147@gmail.com>
2024-03-14 21:45:03 +0800

    lab3: para and error

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -3,3 +3,28 @@ const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+it("test Calculator", ()=>{
+    const calculator = new Calculator();
+    const testCase = [
+        {num:1, exp:Math.exp(1), log: Math.log(1)},
+        {num:10, exp:Math.exp(10), log: Math.log(10)},
+        {num:20, exp:Math.exp(20), log: Math.log(20)}
+    ]
+
+    for(const test of testCase){
+        assert.strictEqual(calculator.exp(test.num), test.exp);
+        assert.strictEqual(calculator.log(test.num), test.log);
+    }
+
+
+    assert.throws(()=>calculator.exp(Number.POSITIVE_INFINITY), {name: 'Error', message: 'unsupported operand type'});
+    assert.throws(()=>calculator.exp(Number.NEGATIVE_INFINITY), {name: 'Error', message: 'unsupported operand type'});
+    assert.throws(()=>calculator.exp(99999), {name: 'Error', message: 'overflow'});
+
+    assert.throws(()=>calculator.log(Number.POSITIVE_INFINITY), {name: 'Error', message: 'unsupported operand type'});
+    assert.throws(()=>calculator.log(Number.NEGATIVE_INFINITY), {name: 'Error', message: 'unsupported operand type'});
+    assert.throws(()=>calculator.log(0), {name: 'Error', message: 'math domain error (1)'});
+    assert.throws(()=>calculator.log(-1), {name: 'Error', message: 'math domain error (2)'});
+
+
+})
