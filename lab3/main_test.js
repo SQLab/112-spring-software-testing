1aa9f2df7b0b8d88ccee877be66adbf16e330e2d
kflin88 <laugh092615@gmail.com>
2024-03-15 01:08:58 +0800

    finish lab3 and reach 100% coverage

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,5 +1,44 @@
-const {describe, it} = require('node:test');
+const { test } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-// TODO: write your tests here
+test ("Test Calculator exp", () => {
+    const calculator = new Calculator();
+    const testcases = [
+        { param: 1, expected: Math.exp(1) },
+        { param: 2, expected: Math.exp(2) },
+        { param: 3, expected: Math.exp(3) },
+    ];
+    for (const tc of testcases) {
+        assert.strictEqual(calculator.exp(tc.param), tc.expected);
+    }
+
+    const error_cases = [
+        { param: 'aa', expected: 'unsupported operand type' },
+        { param: 1000, expected: 'overflow' },
+    ];
+    for (const tc of error_cases) {
+        assert.throws(() => {calculator.exp(tc.param)}, {message: tc.expected});
+    }
+});
+
+test ("Test Calculator log", () => {
+    const calculator = new Calculator();
+    const testcases = [
+        { param: 1, expected: Math.log(1) },
+        { param: 2, expected: Math.log(2) },
+        { param: 3, expected: Math.log(3) },
+    ];
+    for (const tc of testcases) {
+        assert.strictEqual(calculator.log(tc.param), tc.expected);
+    }
+
+    const error_cases = [
+        { param: 'aa', expected: 'unsupported operand type' },
+        { param: 0, expected: 'math domain error (1)' },
+        { param: -1, expected: 'math domain error (2)' },
+    ];
+    for (const tc of error_cases) {
+        assert.throws(() => {calculator.log(tc.param)}, {message: tc.expected});
+    }
+});
