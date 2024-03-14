6ded19a574aabd0fef341d3a92b93b02ff691099
martintl25 <martin.tl.25.cs12@nycu.edu.tw>
2024-03-14 16:50:25 +0800

    Complete testing code. Coverage reaches 100%.

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,4 +2,53 @@ const {describe, it} = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-// TODO: write your tests here
+describe('Test the class `Caluculator`', () => {
+    describe('Test exp(x)', () => {
+        it('should throw errors', () => {
+            const instance = new Calculator();
+            const testcases = [
+                {param: [Infinity], expected: {name: 'Error', message: 'unsupported operand type'}},
+                {param: [1e+10], expected: {name: 'Error', message: 'overflow'}},
+            ];
+            for (const tc of testcases) {
+                assert.throws(() => {instance.exp.apply(this, tc.param);}, tc.expected);
+            }
+        });
+        it('should throw no error', () => {
+            const instance = new Calculator();
+            const testcases = [
+                {param: [0], expected: 1},
+                {param: [1], expected: 2.718281828459045},
+                {param: [-1], expected: 0.36787944117144233},
+            ];
+            for (const tc of testcases) {
+                assert.strictEqual(instance.exp.apply(this, tc.param), tc.expected);
+            }
+        });
+    });
+
+    describe('Test log(x)', () => {
+        it('should throw errors', () => {
+            const instance = new Calculator();
+            const testcases = [
+                {param: [Infinity], expected: {name: 'Error', message: 'unsupported operand type'}},
+                {param: [0], expected: {name: 'Error', message: 'math domain error (1)'}},
+                {param: [-1], expected: {name: 'Error', message: 'math domain error (2)'}}
+            ];
+            for (const tc of testcases) {
+                assert.throws(() => {instance.log.apply(this, tc.param);}, tc.expected);
+            }
+        });
+        it('should throw no error', () => {
+            const instance = new Calculator();
+            const testcases = [
+                {param: [1], expected: 0},
+                {param: [2.718281828459045], expected: 1},
+                {param: [0.36787944117144233], expected: -1},
+            ];
+            for (const tc of testcases) {
+                assert.strictEqual(instance.log.apply(this, tc.param), tc.expected);
+            }
+        });
+    });
+}); 
