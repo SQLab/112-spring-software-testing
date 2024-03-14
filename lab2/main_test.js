73c33b7cb0a5ce6508d4425277895f416ccf05c4
tsaiet <ettsai98@gmail.com>
2024-03-14 21:36:32 +0800

    100% coverage

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,12 +1,12 @@
-const {mock, test} = require('node:test');
-const assert = require('assert');
-const { Application, MailSystem } = require('./main');
-
 const fs = require("fs");
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
+const test = require("node:test");
+const assert = require("assert");
 
-const data = 'Apple\nBanana\nCockroach';
+test.mock.method(fs, "readFile", (path, encoding, callback) => {
+    callback(null, "Apple\nBanana\nCockroach");
+});
+
+const { Application, MailSystem } = require("./main");
 
 test("Test MailSystem's write", () => {
 	const ms1 = new MailSystem();
@@ -16,32 +16,28 @@ test("Test MailSystem's write", () => {
 	
 	assert.strictEqual(ms1.write(name1), "Congrats, Dinosaur!");
 	assert.strictEqual(ms2.write(name2), "Congrats, Pisa!");
+	assert.strictEqual(ms2.write(87), "Congrats, 87!");
+	assert.strictEqual(ms2.write(true), "Congrats, true!");
 });
 
 test("Test MailSystem's send", () => {
 	const ms = new MailSystem();
+	const mock_fn = test.mock.fn(ms.send);
+	const OriginalMathRandom = Math.random;
 	const name = "Pisa";
 	const context = "Congrats, Pisa!";
-	const OriginalMathRandom = Math.random;
 	
 	// Mock
 	test.mock.method(Math, "random", () => 0.6); // Let Math.random() always return 0.6
-	assert.strictEqual(ms.send(name, context), true);
+	assert.strictEqual(mock_fn(name, context), true);
 	test.mock.method(Math, "random", () => 0.4); // Let Math.random() always return 0.4
-	assert.strictEqual(ms.send(name, context), false);
+	assert.strictEqual(mock_fn(name, context), false);
 	
 	// Reset Math.random
 	Math.random = OriginalMathRandom;
 });
 
 test("Test Application's getNames", async () => {
-	
-	await fs.writeFile('name_list.txt', data, 'utf8', (err) => {
-		if (err) {
-			console.log(err);
-		}
-		console.log("Write file success");
-	});
 	const ap = new Application();
 	const [people, selected] = await ap.getNames();
 	assert.deepStrictEqual(people, ['Apple', 'Banana', 'Cockroach']);
@@ -51,15 +47,8 @@ test("Test Application's getNames", async () => {
 test("Test Application's getRandomPerson", async () => {
 	const OriginalMathRandom = Math.random;
 	
-	await fs.writeFile('name_list.txt', data, 'utf8', (err) => {
-		if (err) {
-			console.log(err);
-		}
-		console.log("Write file success");
-	});
-	
 	const ap = new Application();
-	const [people, selected] = await ap.getNames();
+	ap.people = ['Apple', 'Banana', 'Cockroach'];
 	
 	// Mock
 	test.mock.method(Math, "random", () => 0.1); // Let Math.random() always return 0.1
@@ -80,8 +69,27 @@ test("Test Application's selectNextPerson", () => {
 	
 	// Stub
 	ap.people = ['Apple', 'Banana', 'Cockroach'];
-	ap.selected = ['Apple', 'Banana'];
+	ap.selected = ['Apple'];
+	let i = 0;
+	test.mock.method(ap, 'getRandomPerson', () => {
+		if (i == 0) {
+			i++;
+			return 'Apple';
+		}
+		else if (i == 1) {
+			i++;
+			return 'Banana';
+		}
+		else{
+			return 'Cockroach';
+		}
+	})
+	assert.strictEqual(ap.selectNextPerson(), 'Banana');
+	assert.deepStrictEqual(ap.selected, ['Apple', 'Banana']);
+	
 	assert.strictEqual(ap.selectNextPerson(), 'Cockroach');
+	assert.deepStrictEqual(ap.selected, ['Apple', 'Banana', 'Cockroach']);
+	
 	assert.strictEqual(ap.selectNextPerson(), null);
 });
 
@@ -90,20 +98,12 @@ test("Test Application's notifySelected", () => {
 	
 	// Stub
 	ap.people = ['Apple', 'Banana', 'Cockroach'];
-	ap.selected = ['Apple', 'Banana'];
+	ap.selected = ['Apple', 'Banana', 'Cockroach'];
 	
 	// Mock
-	const ms_wr = mock.method(ap.mailSystem, "write");
-	const ms_se = mock.method(ap.mailSystem, "send");
-	
-	var st = "";
-	mock.method(console, 'log', (s) => {
-		st += s + "\n";
-	});
-	test.mock.method(Math, "random", () => 0.9);
+	ap.mailSystem.send = test.mock.fn(ap.mailSystem.send);
+	ap.mailSystem.write = test.mock.fn(ap.mailSystem.write);
 	ap.notifySelected();
-	assert.strictEqual(ms_wr.mock.calls.length, ap.selected.length);
-	assert.strictEqual(ms_se.mock.calls.length, ap.selected.length);
-	
-	assert.strictEqual(st, "--notify selected--\n--write mail for Apple--\n--send mail to Apple--\nmail sent\n--write mail for Banana--\n--send mail to Banana--\nmail sent\n");
+	assert.strictEqual(ap.mailSystem.send.mock.calls.length, 3);
+	assert.strictEqual(ap.mailSystem.write.mock.calls.length, 3);
 });
