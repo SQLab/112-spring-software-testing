1fb692d4557d5e5e3beb1c133b27ca52204c9571
JimYou <45849611+Jim890227@users.noreply.github.com>
2024-03-17 00:37:33 +0800

    Add files via upload

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -3,3 +3,108 @@ const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+describe('calculator', () => {
+    it('exponent normal result', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: 1, expected: Math.exp(1)},
+            { param: 2, expected: Math.exp(2)},
+            { param: 3, expected: Math.exp(3)},
+        ];
+        for (const tc of testcases)
+        {
+            assert.strictEqual(calculator.exp(tc.param), tc.expected);
+        }
+    });
+    it('exponent input is not a number', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: '1'},
+            { param: '2'},
+            { param: '3'},
+        ];
+        const fn = (tc) => { 
+            calculator.exp(tc);
+        };
+        assert.throws(() =>{
+            for (const tc of testcases)
+            {
+                fn(tc.param);
+            }
+        }, Error);
+    });
+    it('exponent result infinity', () =>{
+        const calculator = new Calculator();
+        const testcases = [
+            { param: 1000},
+            { param: 2000},
+            { param: 3000},
+        ];
+        const fn = (tc) => { calculator.exp(tc); };
+        assert.throws(() => {
+            for (const tc of testcases)
+            {
+                fn(tc.param);
+            }
+        }, Error);
+    });
+    it('log input is not a number', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: '1'},
+            { param: '2'},
+            { param: '3'},
+        ];
+        const fn = (tc) => { calculator.log(tc); };
+        assert.throws(() => {
+            for (const tc of testcases)
+            {
+                fn(tc.param);
+            }
+        }, Error);
+    });
+    it('log normal result', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: 1, expected: Math.log(1)},
+            { param: 2, expected: Math.log(2)},
+            { param: 3, expected: Math.log(3)},
+        ];
+        for (const tc of testcases)
+        {
+            assert.strictEqual(calculator.log(tc.param), tc.expected);
+        }
+    });
+    it('log result is NaN', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: -1},
+            { param: -2},
+            { param: -3},
+        ];
+        const fn = (tc) => {
+            calculator.log(tc);
+        };
+        assert.throws(() => {
+            for (const tc of testcases)
+            {
+                fn(tc.param);
+            }
+        }, Error)
+    });
+    it('log result is -Infinity', () => {
+        const calculator = new Calculator();
+        const testcases = [
+            { param: 0},
+        ];
+        const fn = (tc) => {
+            calculator.log(tc);
+        }
+        assert.throws(() => {
+            for (const tc of testcases)
+            {
+                fn(tc.param);
+            }
+        }, Error);
+    });
+});
