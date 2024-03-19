d8d8976e1155d467d3d84be9df1e3fa31168934d
AxelHowe <s11242586@gmail.com>
2024-03-19 12:10:05 +0800

    feat: finish lab3

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,5 +1,57 @@
-const {describe, it} = require('node:test');
+const { describe, it } = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+
+describe('Calculator', () => {
+    describe('exp', () => {
+
+        const testcase = [
+            [0, 1],
+            [1, 2.718281828459045],
+            [-1, 0.36787944117144233],
+            [Infinity, Error],
+            [1000, Error]
+        ];
+
+        testcase.forEach(([input, expected]) => {
+            it(`should return ${expected} for ${input}`, () => {
+                const calculator = new Calculator();
+                if (expected === Error) {
+                    assert.throws(() => calculator.exp(input), Error);
+                } else {
+                    assert.strictEqual(calculator.exp(input), expected);
+                }
+            });
+        }
+        );
+
+    });
+
+    describe('log', () => {
+
+        const testcase = [
+            [1, 0],
+            [1000, 6.907755278982137],
+            [Math.E, 1],
+            [0, Error],
+            [-1, Error],
+            [Infinity, Error]
+        ];
+
+        testcase.forEach(([input, expected]) => {
+            it(`should return ${expected} for ${input}`, () => {
+                const calculator = new Calculator();
+                if (expected === Error) {
+                    assert.throws(() => calculator.log(input), Error);
+                } else {
+                    assert.strictEqual(calculator.log(input), expected);
+                }
+            });
+        }
+        );
+
+    });
+}
+);
