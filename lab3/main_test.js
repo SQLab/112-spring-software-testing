ee322dae51110acf134e29784a5ec56c1bee55e1
DrVerylong <a358970@gmail.com>
2024-03-21 02:18:34 +0800

    2nd try

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -6,38 +6,38 @@ const { Calculator } = require('./main');
 describe("test calculator funtions", ()=>{
     it("exp", ()=>{
         const calc = new Calculator;
-        const testcase = [
-            {param: 0, ans: Math.exp(0)},
-            {param: 5, ans: Math.exp(5)},
-            {param: 10, ans: Math.exp(10)},
+        const correct = [
+            {param: 0, expected: Math.exp(0)},
+            {param: 5, expected: Math.exp(5)},
+            {param: 10, expected: Math.exp(10)},
         ];
-        for(const tc of testcase)
-            assert.strictEqual(calc.exp(tc.param), tc.ans);
+        for(const tc of correct)
+            assert.strictEqual(calc.exp(tc.param), tc.expected);
 
-        const excep_testcase = [
+        const error = [
             {param:"kelvin", expected: {message: 'unsupported operand type',name: 'Error'}},
             {param:100000, expected: {message: 'overflow',name: 'Error'}},
         ];
-        for(const tc of excep_testcase)
+        for(const tc of error)
             assert.throws(() => calc.exp(tc.param),tc.expected);
     });
 
     it("log", ()=>{
         const calc = new Calculator;
-        const testcase = [
-            {param: 1, ans: Math.log(1)},
-            {param: 5, ans: Math.log(5)},
-            {param: 10, ans: Math.log(10)},
+        const correct = [
+            {param: 1, expected: Math.log(1)},
+            {param: 5, expected: Math.log(5)},
+            {param: 10, expected: Math.log(10)},
         ];
-        for(const tc of testcase)
-            assert.strictEqual(calc.log(tc.param), tc.ans);
+        for(const tc of correct)
+            assert.strictEqual(calc.log(tc.param), tc.expected);
 
-        const excep_testcase = [
+        const error = [
             {param:"kelvin", expected: {message: 'unsupported operand type',name: 'Error'}},
             {param:10^10, expected: {message: 'math domain error (1)',name: 'Error'}},
             {param:-1, expected: {message: 'math domain error (2)',name: 'Error'}},
         ];
-        for(const tc of excep_testcase)
+        for(const tc of error)
             assert.throws(() => {calc.log(tc.param)}, tc.expected);
     });
 
