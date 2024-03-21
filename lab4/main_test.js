43b713d94e3b0b7c4ee6f4e56bf47a11365033ca
nayeonyebo <kyle041498@gmail.com>
2024-03-21 18:04:02 +0800

    passed
    finished lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -7,7 +7,20 @@ const puppeteer = require('puppeteer');
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
+    const searchButtonSelector = 'button.DocSearch.DocSearch-Button';
+    const button = await page.waitForSelector(searchButtonSelector);
+    await button.click();
 
+    const inputSelector = 'input.DocSearch-Input';
+    const inputBox = await page.waitForSelector(inputSelector);
+    inputBox.focus();
+    await inputBox.type('chipi chipi chapa chapa');
+    const resultSelector = 'li#docsearch-item-5.DocSearch-Hit';
+    const result = await page.waitForSelector(resultSelector);
+    await result.click();
+    const titleSelector = await page.waitForSelector('text/Experimental WebDriver BiDi support');
+    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
+    console.log(fullTitle);
     // Hints:
     // Click search button
     // Type into search box
