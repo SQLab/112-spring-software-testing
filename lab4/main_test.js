107045822c322454240508299bb61c0589bfa423
ywChen-NTUST <frank1314168@gmail.com>
2024-03-21 15:47:03 +0800

    Add lab4 test

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,14 +8,28 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
     // Click search button
+    const searchbtnSelector = '.DocSearch-Button-Placeholder';
+    await page.waitForSelector(searchbtnSelector);
+    await page.click(searchbtnSelector);
+
     // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
+    const searchbarSelector = '.DocSearch-Input'
+    await page.waitForSelector(searchbarSelector);
+    await page.type(searchbarSelector, 'chipi chipi chapa chapa', {delay: 100});
+    
     // Click on first result in `Docs` section
+    const firstDocResultSelector = '#docsearch-item-5';
+    await page.waitForSelector(firstDocResultSelector);
+    await page.click(firstDocResultSelector);
+
     // Locate the title
+    const titleSelector = '.theme-doc-markdown > h1';
+    const titleElement = await page.waitForSelector(titleSelector);
+    const title = await titleElement?.evaluate(el => el.textContent);
+
     // Print the title
+    console.log(title);
 
     // Close the browser
     await browser.close();
