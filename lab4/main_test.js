06e55e406ca6b20ddd0295b097c3a4866d85da5e
nayeonyebo <kyle041498@gmail.com>
2024-03-21 18:04:35 +0800

    delete comment
    as title

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,11 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
     const searchButtonSelector = 'button.DocSearch.DocSearch-Button';
     const button = await page.waitForSelector(searchButtonSelector);
@@ -21,15 +19,5 @@ const puppeteer = require('puppeteer');
     const titleSelector = await page.waitForSelector('text/Experimental WebDriver BiDi support');
     const fullTitle = await titleSelector?.evaluate(el => el.textContent);
     console.log(fullTitle);
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
-
-    // Close the browser
     await browser.close();
 })();
