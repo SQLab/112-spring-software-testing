319b8032770175bcc2359651acb6f62f7df3d895
ian60509 <ian.60509@gmail.com>
2024-03-21 16:25:22 +0000

    finish!

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,30 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.waitForSelector('.DocSearch-Button-Placeholder');
+    await page.click('.DocSearch-Button-Placeholder');
+
     // Type into search box
+    const inputSearchSeletor = '#docsearch-input';
+    const inputStr = 'chipi chipi chapa chapa'
+    await page.waitForSelector(inputSearchSeletor);
+    await page.type(inputSearchSeletor, inputStr, {delay:100});
+
     // Wait for search result
     // Get the `Docs` result section
     // Click on first result in `Docs` section
     // Locate the title
-    // Print the title
+    const docsFirstItemSelector = '#docsearch-item-5';
+    await page.waitForSelector(docsFirstItemSelector);
+    await page.click(docsFirstItemSelector);
 
+    const titleSelectorName = 'h1';
+    const titleSelector = await page.waitForSelector(titleSelectorName);
+    const titleText = await titleSelector?.evaluate(el => el.textContent);
+
+
+    // Print the title
+    console.log(titleText)
     // Close the browser
     await browser.close();
 })();
