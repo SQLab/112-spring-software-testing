bf319492685974923cf542c53c37c088ca3ed80d
gg-blog <andydaniel146@gmail.com>
2024-03-22 12:29:56 +0800

    finish

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,24 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.waitForSelector('button.DocSearch.DocSearch-Button');
+    await page.click('button.DocSearch.DocSearch-Button');
     // Type into search box
+    await page.waitForSelector('.DocSearch-Input');
+    await page.type('.DocSearch-Input','chipi chipi chapa chapa');
     // Wait for search result
     // Get the `Docs` result section
     // Click on first result in `Docs` section
+    const searchResultSelector = 'body > div:nth-child(1) > div > div > div > div > section:nth-child(2) > ul > li > a ';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
     // Locate the title
     // Print the title
-
+    const textSelector = await page.waitForSelector(
+    '.theme-doc-markdown.markdown > h1'
+    );
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
     // Close the browser
+    console.log(fullTitle);
     await browser.close();
 })();
