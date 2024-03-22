9faada97569fb084540da02ce9f54cb1a2646f18
nayeonyebo <kyle041498@gmail.com>
2024-03-22 12:23:06 +0800

    finished lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,23 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
+    const searchButtonSelector = 'button.DocSearch.DocSearch-Button';
+    const button = await page.waitForSelector(searchButtonSelector);
+    await button.click();
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
-
-    // Close the browser
+    const inputSelector = 'input.DocSearch-Input';
+    const inputBox = await page.waitForSelector(inputSelector);
+    inputBox.focus();
+    await inputBox.type('chipi chipi chapa chapa');
+    const resultSelector = 'li#docsearch-item-5.DocSearch-Hit';
+    const result = await page.waitForSelector(resultSelector);
+    await result.click();
+    const titleSelector = await page.waitForSelector('text/Experimental WebDriver BiDi support');
+    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
+    console.log(fullTitle);
     await browser.close();
 })();
