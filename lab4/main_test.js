7f6ec4b1b8d63b1f2c79bddd007acbcfc44bebe3
meng-yu-chen <84662006+meng-yu-chen@users.noreply.github.com>
2024-03-22 18:19:09 +0800

    use puppeteer to test website

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,3 +1,4 @@
+// use puppeteer package for web test
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -8,14 +9,34 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
+    // Click search button 
+    const serach_button_selector = 'button.DocSearch.DocSearch-Button[aria-label="Search"]';
+    await page.waitForSelector(serach_button_selector);
+    await page.click(serach_button_selector);    
+
+    // Type into search box 
+    const search_box_sector = '#docsearch-input';
+    await page.waitForSelector(search_box_sector);
+
+    // Put keyboard input to search box
+    await page.keyboard.type('chipi chipi chapa chapa');
+
+    // Wait for search result 
+    await page.type(search_box_sector, 'chipi chipi chapa chapa', { delay: 300 });
+
+    // Get first result in `Docs` section
+    const search_result_selector = "[id='docsearch-item-5']";
+    await page.waitForSelector(search_result_selector);
+    
     // Click on first result in `Docs` section
+    await page.click(search_result_selector);
+
     // Locate the title
+    const title_selector = await page.waitForSelector('div.theme-doc-markdown.markdown h1');
+    const title = await title_selector?.evaluate( element => element.textContent );
+
     // Print the title
+    console.log(title);
 
     // Close the browser
     await browser.close();
