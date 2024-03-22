477677c6efeacb9e8b027de946957c479c8681f9
Yu-Rou-Weng <108703042@nccu.edu.tw>
2024-03-22 11:46:03 +0800

    submit lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,27 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const searchResultSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
     // Type into search box
+    const InputSelector = '.DocSearch-Input';  //form.DocSearch-Form input#docsearch-input
+    await page.waitForSelector(InputSelector);
+    await page.type(InputSelector, 'chipi chipi chapa chapa', { delay: 700 });
     // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    const ResultSelector = '.DocSearch-Hit-title'
 
+    // Get the `Docs` result section
+    await page.waitForSelector(ResultSelector)
+    // Click on first result in `Docs` section
+    await page.click(ResultSelector)
+    // Locate the title
+    const textSelector = await page.waitForSelector(
+        'text/Experimental WebDriver'
+    );
+    const Title = await textSelector?.evaluate(el => el.textContent);
+    // Print the title
+    console.log(Title)
     // Close the browser
     await browser.close();
 })();
