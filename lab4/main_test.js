167fe480e7f76dfc20398b388715c59064f9827b
encumbrance <audi15420@gmail.com>
2024-03-22 21:37:15 +0800

    modify the lab4/main_test.js file

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,12 +10,28 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const searchButton =  ".DocSearch-Button-Placeholder";
+    await page.waitForSelector(searchButton);
+    await page.click(searchButton);
+   
     // Type into search box
+    await page.waitForSelector('.DocSearch-Input');
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 500});  
+
     // Wait for search result
+    //select the section where the search result is docs
     // Get the `Docs` result section
     // Click on first result in `Docs` section
+    const searchResult = "#docsearch-item-5"
+    const first_result = await page.waitForSelector(searchResult);
+    await first_result.click();
+   
+   
     // Locate the title
+    const textselector = await page.waitForSelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div > div > article > div.theme-doc-markdown.markdown > h1');
+    const text = await page.evaluate(textselector => textselector.textContent, textselector);
     // Print the title
+    console.log(text);
 
     // Close the browser
     await browser.close();
