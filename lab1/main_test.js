985a7ba4e6d2791de8606cd7dd2994f8215d98a7
SUNGOD3 <sun1223god@gmail.com>
2024-03-24 04:03:13 +0800

    lab1 recover

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -4,50 +4,20 @@ const { MyClass, Student } = require('./main');
 
 test("Test MyClass's addStudent", () => {
     // TODO
-    const myClass = new MyClass();
-    const student = new Student();
-    student.setName('SUNGOD');
-    assert.strictEqual(myClass.addStudent(student), 0);
-    const student2 = new Student();
-    student2.setName('');
-    assert.strictEqual(myClass.addStudent(student2), 1);
-    assert.strictEqual(myClass.addStudent(student2), 2);
-    assert.strictEqual(myClass.addStudent('YJ'), -1);
-    assert.strictEqual(myClass.addStudent(0), -1);
-    assert.strictEqual(myClass.addStudent(), -1);
-    //throw new Error("Test not implemented");
+    throw new Error("Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
     // TODO
-    const myClass = new MyClass();
-    const student = new Student();
-    student.setName('SUNGOD');
-    const newStudentId = myClass.addStudent(student);
-    assert.strictEqual(myClass.getStudentById(newStudentId), student);
-    //testing fail
-    //assert.strictEqual(myClass.getStudentById('John'), null);
-    assert.strictEqual(myClass.getStudentById(-1), null);
-    //throw new Error("Test not implemented");
+    throw new Error("Test not implemented");
 });
 
 test("Test Student's setName", () => {
     // TODO
-    const myClass = new MyClass();
-    const student = new Student();
-    student.setName('SUNGOD');
-    assert.strictEqual(student.name, 'SUNGOD');
-    student.setName(123); //wrong type
-    assert.strictEqual(student.name, 'SUNGOD');
-    //throw new Error("Test not implemented");
+    throw new Error("Test not implemented");
 });
 
 test("Test Student's getName", () => {
     // TODO
-    const myClass = new MyClass();
-    const student = new Student();
-    assert.strictEqual(student.getName(), '');
-    student.setName('SUNGOD');
-    assert.strictEqual(student.getName(), 'SUNGOD');
-    //throw new Error("Test not implemented");
+    throw new Error("Test not implemented");
 });
