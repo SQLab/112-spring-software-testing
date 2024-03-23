76a16003107bce735375c664bc4c3a91e81ec2f8
SUNGOD3 <sun1223god@gmail.com>
2024-03-24 03:16:51 +0800

    commit lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -2,21 +2,27 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
-
     // Hints:
     // Click search button
+    await page.click('.DocSearch-Button-Placeholder');
+    await new Promise(resolve => setTimeout(resolve, 1000));
     // Type into search box
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
     // Wait for search result
+    await new Promise(resolve => setTimeout(resolve, 1000));
     // Get the `Docs` result section
+    await page.click('#docsearch-item-5');
     // Click on first result in `Docs` section
+    await new Promise(resolve => setTimeout(resolve, 1000));
     // Locate the title
+    const title = await page.title();
     // Print the title
-
+    console.log(title);
     // Close the browser
     await browser.close();
 })();
