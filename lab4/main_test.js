8379b84f25e17735f7f5e56531307121d744bc25
JimYou <45849611+Jim890227@users.noreply.github.com>
2024-03-23 22:19:59 +0800

    Add files via upload

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,8 +10,18 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    await page.waitForSelector('.DocSearch-Button-Placeholder');
+    await page.click('.DocSearch-Button-Placeholder');
     // Type into search box
+    await page.waitForSelector('.DocSearch-Input');
+    await page.type('.DocSearch-Input','chipi chipi chapa chapa', {delay: 1000});
+    await page.waitForSelector('#docsearch-item-5 > a > div > div.DocSearch-Hit-content-wrapper > span.DocSearch-Hit-title');
+    await page.click('#docsearch-item-5 > a > div > div.DocSearch-Hit-content-wrapper > span.DocSearch-Hit-title');
+    const textselect = await page.waitForSelector('#__docusaurus_skipToContent_fallback > div > div > main > div > div > div > div > article > div.theme-doc-markdown.markdown > h1');
+    const result = await textselect?.evaluate(el => el.textContent);
+    console.log(result);
     // Wait for search result
+
     // Get the `Docs` result section
     // Click on first result in `Docs` section
     // Locate the title
