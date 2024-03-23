90f4154d466e30dce998d78e5fd5113a1ed697f6
tunajaw <t.a.w.forwork@gmail.com>
2024-03-23 14:15:56 +0800

    complete lab 4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,23 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const searchSelector = '.DocSearch-Button-Placeholder';
+    // await page.waitForSelector(searchSelector);
+    await page.click(searchSelector);
+    await page.waitForSelector(searchSelector);
     // Type into search box
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa', {delay: 500});
     // Wait for search result
     // Get the `Docs` result section
     // Click on first result in `Docs` section
+    const searchResultSelector = '#docsearch-item-5';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
     // Locate the title
+    const textSelector = await page.waitForSelector('h1');
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
     // Print the title
-
+    console.log(fullTitle);
     // Close the browser
     await browser.close();
 })();
