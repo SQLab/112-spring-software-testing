c55d767fcdba6f71019c9a9820e36f93a535b672
TAI-YANG <72863539+SUNGOD3@users.noreply.github.com>
2024-03-24 16:34:31 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -2,31 +2,25 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     // Launch the browser and open a new blank page
-
-    const browser = await puppeteer.launch();
-
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
 
     // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
-
+    await page.goto('https://pptr.dev/', { delay: 1000 });
     // Hints:
     // Click search button
-    await page.click('.DocSearch-Button-Placeholder');
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await page.click('.DocSearch-Button-Placeholder', { delay: 1000 });
     // Type into search box
-    await page.type('#docsearch-input', 'chipi chipi chapa chapa');
+    await page.type('#docsearch-input', 'chipi chipi chapa chapa', { delay: 100 });
     // Wait for search result
-    await new Promise(resolve => setTimeout(resolve, 1000));
     // Get the `Docs` result section
-    await page.click('#docsearch-item-5');
+    await page.click('#docsearch-item-5', { delay: 1000 });
     // Click on first result in `Docs` section
-    await new Promise(resolve => setTimeout(resolve, 1000));
     // Locate the title
+    await page.waitForSelector('.docItemContainer_Djhp');
     const title = await page.title();
     // Print the title
     console.log(title);
-
     // Close the browser
     await browser.close();
 })();
