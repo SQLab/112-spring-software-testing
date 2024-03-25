29d78a2211aec5404349b8e4d1c2673e2f348d68
nyeee0622 <nyeee.cs12@nycu.edu.tw>
2024-03-25 16:29:06 +0800

    only lab1

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -2,22 +2,32 @@ const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
 
+const cs = new MyClass();
+const john = new Student();
+john.setName("John");
+var id;
+
 test("Test MyClass's addStudent", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    assert(cs.addStudent("John") == -1, "Test not implemented");
+    assert(cs.addStudent(john) != -1, "Test not implemented");
 });
 
 test("Test MyClass's getStudentById", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    id = cs.addStudent(john);
+    assert(cs.getStudentById(-1) == null, "Test not implemented");
+    assert(cs.getStudentById(id+1) == null, "Test not implemented");
+    assert(cs.getStudentById(id) == john, "Test not implemented");
 });
 
+const st = new Student();
+const st2 = new Student();
+
 test("Test Student's setName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    st.setName(123);
+    st2.setName("John");
 });
 
 test("Test Student's getName", () => {
-    // TODO
-    throw new Error("Test not implemented");
+    assert(st.getName() == '', "Test not implemented");
+    assert(st2.getName() == 'John', "Test not implemented"); 
 });
