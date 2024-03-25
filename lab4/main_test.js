63ab5d74bb28ed6d5dc3149ac3b4207eaa260ef1
HaKkaz <luckyanthonyan@gmail.com>
2024-03-25 09:35:26 +0800

    finished lab4

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,12 +10,44 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    try {
+        // Wait for the button to be present in the DOM
+        const searchButton = '.DocSearch.DocSearch-Button';
+        await page.waitForSelector(searchButton);
+        await page.click(searchButton);
+    } catch (error) {
+        console.error('Button not found or could not be clicked:', error);
+    }
+
     // Type into search box
+    try {
+        // Wait for the search box to be present in the DOM
+        await page.waitForSelector('.DocSearch-Input');
+
+        // Type into the search box
+        await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 500});  
+    } catch (error) {
+        console.error('Search box not found or could not be typed into:', error);
+    }
+
     // Wait for search result
     // Get the `Docs` result section
     // Click on first result in `Docs` section
+    try {
+        // Wait for the search result to be present in the DOM
+        const searchResult = "[id='docsearch-item-5']";
+        await page.waitForSelector(searchResult);
+        await page.click(searchResult);
+    } catch (error) {
+        console.error('Search result button not found:', error);
+    }
+    
     // Locate the title
+    const textselector = await page.waitForSelector('.markdown h1:first-child');
+    const title = await page.evaluate(textselector => textselector.textContent, textselector);
+
     // Print the title
+    console.log(title);
 
     // Close the browser
     await browser.close();
