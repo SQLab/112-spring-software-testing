19a37fff501533c54269f8b79dce0f5aeffc7077
DrVerylong <a358970@gmail.com>
2024-03-28 11:32:55 +0800

    pass

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,13 +10,33 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
+    const searchButtonSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchButtonSelector);
+    await page.click(searchButtonSelector);
     // Type into search box
+    const searchInput = 'chipi chipi chapa chapa';
+    const searchInputSelector = '.DocSearch-Input';
+    await page.waitForSelector(searchInputSelector);
+    await page.type(searchInputSelector, searchInput, { delay: 700 }); 
     // Wait for search result
     // Get the `Docs` result section
     // Click on first result in `Docs` section
+    await page.waitForSelector('#docsearch-item-5');
+    await page.click('#docsearch-item-5');
     // Locate the title
     // Print the title
 
+
+
+
+    const titleElement = await page.waitForSelector('.theme-doc-markdown > h1');
+    const title = await titleElement?.evaluate(el => el.textContent);
+
+    // 打印标题到控制台
+    console.log(title);
+
+    // 关闭浏览器
+    await browser.close();
     // Close the browser
     await browser.close();
 })();
