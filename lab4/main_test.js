be764b34ad23cbca88471baf1c59eb4184dfa717
YingMuo <wl03452329@gmail.com>
2024-04-18 13:46:40 +0800

    feat: merge lab5

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,38 +1,22 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto("https://pptr.dev/");
+    // Navigate the page to a URL
+    await page.goto('https://pptr.dev/');
 
-  // Click search button
-  await page.click(".DocSearch-Button-Placeholder");
+    // Hints:
+    // Click search button
+    // Type into search box
+    // Wait for search result
+    // Get the `Docs` result section
+    // Click on first result in `Docs` section
+    // Locate the title
+    // Print the title
 
-  // Type into search box
-  const input = "#docsearch-input";
-  await page.waitForSelector(input);
-  await page.type(input, "chipi chipi chapa chapa");
-
-  // Wait for search result
-  // Get the `Docs` result section
-  const searchResultSelector = "#docsearch-item-5";
-  await page.waitForSelector(searchResultSelector);
-
-  // Click on first result in `Docs` section
-  await page.click(searchResultSelector);
-
-  // Locate the title
-  const textSelector = await page.waitForSelector(
-    "text/Experimental WebDriver BiDi support"
-  );
-  const fulltitle = await textSelector?.evaluate((el) => el.textContent);
-
-  // Print the title
-  console.log(fulltitle);
-
-  // Close the browser
-  await browser.close();
+    // Close the browser
+    await browser.close();
 })();
