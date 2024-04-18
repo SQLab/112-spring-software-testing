0343f93e2b056b4097831b16aff127b0a8aa5e86
YingMuo <wl03452329@gmail.com>
2024-04-18 13:46:39 +0800

    feat: merge lab5

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -2,41 +2,4 @@ const {describe, it} = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-describe('exp', () => {
-    const valid_testcase = [
-        {inputvalue: 1, expected: Math.exp(1)},
-        {inputvalue: -1, expected: Math.exp(-1)},
-        {inputvalue: 0, expected: Math.exp(0)}
-    ];
-    const invalid_testcase = [
-        {inputvalue: 9999999999, expected: {name: 'Error', message: 'overflow'}},
-        {inputvalue: NaN, expected: {name: 'Error', message: 'unsupported operand type'}},
-        {inputvalue: 1000, expected: {name: 'Error', message: 'overflow'}}
-    ];
-    const calculator = new Calculator();
-
-    for(const tc of valid_testcase)
-        assert.strictEqual(calculator.exp(tc.inputvalue) , tc.expected);
-    for(const tc of invalid_testcase)
-        assert.throws(() => calculator.exp(tc.inputvalue), new Error(tc.expected.message)) ;
-});
-
-describe('log', () => {
-    const valid_testcase = [
-        {inputvalue: 100, expected: Math.log(100) },
-        {inputvalue: 0.5, expected: Math.log(0.5) },
-        {inputvalue: 1, expected: Math.log(1) }
-    ];
-    const invalid_testcase = [
-        {inputvalue: Infinity, expected: {name: 'Error', message: 'unsupported operand type'}},
-        {inputvalue: NaN, expected: {name: 'Error', message: 'unsupported operand type'}},
-        {inputvalue: 0, expected: {name: 'Error', message: 'math domain error (1)'}},
-        {inputvalue: -100, expected: {name: 'Error', message: 'math domain error (2)'}}  
-    ];
-    const calculator = new Calculator();
-
-    for (const tc of valid_testcase)
-        assert.strictEqual(calculator.log(tc.inputvalue) , tc.expected);
-    for (const tc of invalid_testcase)
-        assert.throws(() => calculator.log(tc.inputvalue) , new Error(tc.expected.message));
-});
+// TODO: write your tests here
--- lab4/main_test.js
+++ lab4/main_test.js
@@ -17,29 +17,6 @@ const puppeteer = require('puppeteer');
     // Locate the title
     // Print the title
 
-    // Click search button
-    const button = '.DocSearch.DocSearch-Button';
-    await page.waitForSelector(button);
-    await page.click(button);
-
-    // Type into search box
-    await page.waitForSelector('.DocSearch-Input');
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 500}); 
-
-    // Get the `Docs` result section
-    const res = "[id='docsearch-item-5']";
-    await page.waitForSelector(res);
-
-    // Click on first result in `Docs` section
-    await page.click(res);
-
-    // Locate the title
-    const textselector = await page.waitForSelector('.markdown h1:first-child');
-    const text = await page.evaluate(textselector => textselector.textContent, textselector);
-
-    // Print the title
-    console.log(text);
-
     // Close the browser
     await browser.close();
 })();
