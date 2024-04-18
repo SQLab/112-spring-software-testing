243dc7e250bf21206ba279b1eb155c1275f2f0c1
YingMuo <wl03452329@gmail.com>
2024-04-18 13:46:39 +0800

    feat: merge lab5

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -10,30 +10,12 @@ const puppeteer = require('puppeteer');
 
     // Hints:
     // Click search button
-    const searchbuttonSelector = '.DocSearch-Button';
-    await page.waitForSelector(searchbuttonSelector);
-    await page.click(searchbuttonSelector);
-
     // Type into search box
     // Wait for search result
-    const searchInputSelector = '.DocSearch-Input';
-    const searchInput = 'chipi chipi chapa chapa';
-    await page.waitForSelector(searchInputSelector);
-    await page.type(searchInputSelector, searchInput, { delay: 500 });
-
     // Get the `Docs` result section
     // Click on first result in `Docs` section
-    const searchDocSelector = '#docsearch-item-5';
-    await page.waitForSelector(searchDocSelector);
-    await page.click(searchDocSelector);
-
     // Locate the title
-
-    const textSelector = await page.waitForSelector('.theme-doc-markdown.markdown h1');
-    const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
     // Print the title
-    console.log(fullTitle);
 
     // Close the browser
     await browser.close();
