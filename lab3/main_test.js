e9108fa667ea5fb3e0216097d6bc7261b41a2649
YingMuo <wl03452329@gmail.com>
2024-04-18 13:46:39 +0800

    feat: merge lab5

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,51 +1,5 @@
-const {describe, it, default: test} = require('node:test');
+const {describe, it} = require('node:test');
 const assert = require('assert');
 const { Calculator } = require('./main');
 
-it("test Caculator's exp", () => {
-    const calculator = new Calculator();
-    var normalCases = [
-        { param: 0, expected: Math.exp(0) },
-        { param: 1, expected: Math.exp(1) },
-        { param: -10, expected: Math.exp(-10) },
-        { param: 5.3, expected: Math.exp(5.3)}
-    ];
-
-    for(let nc of normalCases){
-        assert.strictEqual(calculator.exp(nc.param), nc.expected, 'exp fail');
-    }
-
-    let errCases = [
-        { param : Infinity, exp : 'unsupported operand type'},
-        { param : 10000, exp : 'overflow'},
-    ];
-
-    for(let ec of errCases){
-        assert.throws(()=>calculator.exp(ec.param), Error);
-    }
-
-    
-});
-
-it("test Caculator's log", () => {
-    const calculator = new Calculator();
-    var normalCases = [
-        { param: 1, expected: Math.log(1) },
-        { param: 2, expected: Math.log(2) },
-        { param: 0.00003678794411714447, expected: Math.log(0.00003678794411714447) },
-    ];
-    for(let nc of normalCases){
-        assert.strictEqual(calculator.log(nc.param), nc.expected, 'log fail');
-    }
-
-    let infiniteCases = [
-        { param: -0 },//-Infinity
-        { param: NaN},//!isFinite
-        { param: -5}//isNaN
-    ];
-    for(let ic of infiniteCases){
-        assert.throws(()=>calculator.log(ic.param), Error);
-    }
-
-    
-});
+// TODO: write your tests here
--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,26 +8,15 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
+    // Hints:
     // Click search button
-    const searchButton = '.DocSearch-Button-Placeholder';
-    await page.waitForSelector(searchButton);
-    await page.click(searchButton);
     // Type into search box
-
-    await page.type('.DocSearch-Button-Placeholder', 'chipi chipi chapa chapa', { delay: 500 });
     // Wait for search result
     // Get the `Docs` result section
-    const searchResult = '#docsearch-item-5';
-    await page.waitForSelector(searchResult);
-    await page.click(searchResult);
-
     // Click on first result in `Docs` section
     // Locate the title
-    const h1Selector = await page.waitForSelector('h1');
-    const result = await h1Selector?.evaluate(el => el.textContent);
-    
     // Print the title
-    console.log(result);
+
     // Close the browser
     await browser.close();
 })();
