8072b3fcbf18da83dff0fb104c1e5df086f56b2d
YingMuo <wl03452329@gmail.com>
2024-04-18 13:46:39 +0800

    feat: merge lab5

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -1,53 +1,5 @@
-const test = require('node:test');
+const {describe, it} = require('node:test');
 const assert = require('assert');
-const fs = require('fs');
-
 const { Calculator } = require('./main');
 
-test("Calculator's exp", () => {
-    
-    const calculator = new Calculator();
-
-    const testcases = [
-        {param: 0, expected: 1}, //normal
-        {param: 1, expected: Math.exp(1)}, //normal
-        {param: 2, expected: Math.exp(2)}, //normal
-        {param: Infinity, expected: 'unsupported operand type'}, //if input infinite
-        {param: 'test', expected: 'unsupported operand type'}, //if input not number
-        {param: 1000, expected: 'overflow'} //if output infinite
-    ]
-
-    for (let i = 0; i < testcases.length; i++) {
-        if (i < 3){
-            assert.strictEqual(calculator.exp(testcases[i].param), testcases[i].expected);
-        }
-        else{//exception
-            assert.throws(() => {calculator.exp(testcases[i].param)}, { message: testcases[i].expected });
-        }
-    }
-});
-
-test("Calculator's log", () => {
-   
-    const calculator = new Calculator();
-
-    const testcases = [
-        {param: 1, expected: 0}, //normal
-        {param: Math.exp(1), expected: 1}, //normal
-        {param: 2, expected: Math.log(2)}, //normal
-        {param: Infinity, expected: 'unsupported operand type'}, //if input infinite
-        {param: 'test', expected: 'unsupported operand type'}, //if input not number
-        {param: 0, expected: 'math domain error (1)'}, //if output -infinite
-        {param: -1, expected: 'math domain error (2)'} //if output not number
-    ]
-
-    for (let i = 0; i < testcases.length; i++) {
-        if (i < 3){
-            assert.strictEqual(calculator.log(testcases[i].param), testcases[i].expected);
-        }
-        else{//exception
-            assert.throws(() => {calculator.log(testcases[i].param)}, { message: testcases[i].expected });
-        }
-    }
-});
-
+// TODO: write your tests here
--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,26 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-
+    // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    await page.waitForSelector('.DocSearch-Button');
-    await page.click('.DocSearch-Button');
+    // Hints:
+    // Click search button
+    // Type into search box
+    // Wait for search result
+    // Get the `Docs` result section
+    // Click on first result in `Docs` section
+    // Locate the title
+    // Print the title
 
-    await page.waitForSelector('.DocSearch-Input');
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 1000}); 
-
-    await page.waitForSelector("#docsearch-item-5");
-    await page.click("#docsearch-item-5");
-
-    const titleSelector = await page.waitForSelector('.markdown h1');
-    const title = await page.evaluate(titleSelector => titleSelector.textContent, titleSelector);
-    
-    console.log(title);
-
-    browser.close();
-
+    // Close the browser
+    await browser.close();
 })();
