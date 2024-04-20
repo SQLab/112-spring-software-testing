2541b01bfb4783dcdd161c5b0721a481fda2b58f
hsiangchengfun <610467hc@gmail.com>
2024-04-21 01:56:49 +0800

    lab5 fix antiasan

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,9 +1,6 @@
 // TODO:
 void antiasan(unsigned long addr)
 {
-    //calculate address of shadow memory
     unsigned long *shadow = (unsigned long *)((addr >> 3) + 0x7fff8000);
-    //set to zero as addressable
     *shadow = 0x00;
-
 }
