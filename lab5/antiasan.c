3ef2c875653bf5c21fefaee34ea37834f98761a5
poro777 <55618414+poro777@users.noreply.github.com>
2024-04-20 16:56:46 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,5 @@
-
 void antiasan(unsigned long addr)
 {
-    long long* add = (long long *)((addr >> 3) + 0x7fff8000);
-    *add++ = 0x00;
+    char* add = (char *)((addr >> 3) | 0x7fff8000);
+    *add = 0x00;
 }
