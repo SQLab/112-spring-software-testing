db74914c363fa0e167dbfb899392268411eaa74b
gg-blog <andydaniel146@gmail.com>
2024-04-22 21:00:23 +0800

    finished 1st and 2nd

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,11 @@
-// TODO:
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+__attribute__((no_sanitize("address")))
 void antiasan(unsigned long addr)
 {
-
+	char* ss = (char*)addr;
+	malloc(1<<20);
+        
 }
