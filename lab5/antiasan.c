aeab34d459aae7294d6e4695437d4e63a4a4f0c3
JimYou <45849611+Jim890227@users.noreply.github.com>
2024-04-23 18:21:22 +0800

    Update antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,6 +1,6 @@
 // TODO:
 void antiasan(unsigned long addr)
 {
-    char *shadow = (char*)0x7fff8000 + (addr >> 3);
-    *shadow = 0x00;
+    char *shadow = (char*) (0x7fff8000 + (addr >> 3));
+    *shadow = 0;
 }
