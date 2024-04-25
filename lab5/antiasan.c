066fcdabe338277f152d17fc3f4567c431096727
gg-blog <andydaniel146@gmail.com>
2024-04-25 14:05:14 +0800

    try to fix merge problem

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,13 @@
-// TODO:
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+__attribute__((no_sanitize("address")))
 void antiasan(unsigned long addr)
 {
+	char* ss = (char*)addr;
+	malloc(1<<20);
+        
 
 }
