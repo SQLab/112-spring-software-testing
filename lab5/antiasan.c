a24aef8be0a4e8ac714d3ef21e3e2486a553454a
Carreyliu <carreynycu2618@gmail.com>
2024-04-30 12:15:41 +0000

    Lab5 Pass

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,7 @@
 // TODO:
 void antiasan(unsigned long addr)
 {
-
+    // change the shadow memory for the address addr
+    unsigned long *p = (unsigned long *)((addr >> 3) + 0x7fff8000);
+    *p = 0x00;
 }
