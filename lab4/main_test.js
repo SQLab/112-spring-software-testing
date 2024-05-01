44855ec0ecba16d3e506500e81287a105be59b22
s123tg <56829688+s123tg@users.noreply.github.com>
2024-05-01 14:16:54 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,22 +1,22 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+    // Launch the browser and open a new blank page
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
+    // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    const serachButton='.DocSearch-Button-Placeholder';
-    await page.waitForSelector(serachButton);
-    await page.click(serachButton);
-    await page.waitForSelector('#docsearch-input');
-    await page.type('#docsearch-input','chipi chipi chapa chapa');
-    const pageButton = 'body > div:nth-child(1) > div > div > div > div > section:nth-child(2) > ul > li > a ';
-    const button = await page.waitForSelector(pageButton);
-    try{await page.click(pageButton);}catch(error){console.error(error)}
-    const pageSelector = await page.waitForSelector('.theme-doc-markdown.markdown h1');
-    const title = await pageSelector?.evaluate(element=>element.textContent);
-    console.log(title);
+    // Hints:
+    // Click search button
+    // Type into search box
+    // Wait for search result
+    // Get the `Docs` result section
+    // Click on first result in `Docs` section
+    // Locate the title
+    // Print the title
 
+    // Close the browser
     await browser.close();
 })();
