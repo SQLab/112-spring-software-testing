9e064ea456abbfc1314ba4810271005c9d5fc302
chysong <chysong@cs.nctu.edu.tw>
2024-05-02 06:57:27 +0800

    fix: antiasan.c

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -2,7 +2,7 @@
 void antiasan(unsigned long addr)
 {
     // find shadow 
-    unsigned* shadow = (unsigned*) ( 0x7fff8000 + (addr >> 3));
+    unsigned long* shadow = (unsigned long*) ( 0x7fff8000 + (addr >> 3));
     // destroy the shadow;
     *shadow = 0;
 }
