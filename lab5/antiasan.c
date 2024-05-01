45fb0e65de49dc2861739641a2ab5ae3649ca6ee
samuel <samuel20010308@gamil.com>
2024-05-01 14:29:52 +0800

    set all fd

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,5 +1,6 @@
 void antiasan(unsigned long addr)
 {
-	char* shadow=(addr>>3)+0x7fff8000;
-    *shadow=0; 
+	char* shadow=((addr-0x10)>>3)+0x7fff8000;
+    for(int i=0;i<3;i++)
+        shadow[i]=0; 
 }
