a5c52e9c6a0795f453b79840e433abe14c445750
linhengpei <garylin072190@gmail.com>
2024-05-02 17:16:26 +0800

    finish lab07

---
+++ lab7/sol.py
@@ -0,0 +1,17 @@
+import angr ,sys 
+
+proj = angr.Project('./login') ## create a angr.Project object
+
+def success_condition(state):
+	return b"Login successful" in state.posix.dumps(sys.stdout.fileno())
+
+def fail_condition(state):
+	return b"Login failed" in state.posix.dumps(sys.stdout.fileno())
+
+init_state = proj.factory.entry_state() ## initialize
+
+simulation = proj.factory.simgr(init_state)
+
+simulation.explore(find = success_condition,avoid=fail_condition)
+
+print(simulation.found[0].posix.dumps(sys.stdin.fileno()))
