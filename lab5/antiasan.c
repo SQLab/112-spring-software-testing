91bab19264aacf42fb1137d600181b8f9fd34299
Immypeko <yles.94214@gmail.com>
2024-05-10 07:43:53 +0800

    test: lab5 passing autograding test

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -14,7 +14,7 @@ void antiasan(unsigned long addr)
     
     /* mprotect need to perform on aligned page address, the page size is x^12 bytes */
     unsigned long b_addr = ((rz>>12)<<12);
-    // printf("%p\n%p\n", (unsigned long*)rz, (unsigned long*)b_addr);
+     printf("%p\n%p\n", (unsigned long*)rz, (unsigned long*)b_addr);
 
     /* change the access protection for memory page so that it can be read and write */
     if(mprotect((void*)b_addr, getpagesize(), PROT_READ | PROT_WRITE) == -1) {
@@ -23,8 +23,8 @@ void antiasan(unsigned long addr)
 
     /* change value in shadow to which s[0x10] map */
     /* first printf: fafafafafafafafd, second printf: fafafafafafafa00 */
-    // printf("%lx\n", *(unsigned long*)rz);
+     printf("%lx\n", *(unsigned long*)rz);
     *(char*)rz = 0x0;
-    // printf("%lx\n", *(unsigned long*)rz);
+     printf("%lx\n", *(unsigned long*)rz);
 }
 // 0xc067fff8002: 0xfafafafafafdfdfd
